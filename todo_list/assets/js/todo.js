4f711c6752ef911dbcdb8cc195cc1816e3248739
Angela Shen <angelashen@angelas-mbp.lan>
2018-05-09 20:36:45 -0700

    finish styling

--- todo_list/assets/js/todo.js
+++ todo_list/assets/js/todo.js
@@ -5,7 +5,7 @@ $("ul").on("click", "li", function(){
 
 // delete todos by clicking trashcan
 $("ul").on("click", "span", function(e){
-	$(this).parent().fadeOut(500, function(){
+	$(this).parent().fadeOut(300, function(){
 		$(this).remove()
 	});
 	// stops event from bubbling up to parent elements
@@ -20,8 +20,13 @@ $("input[type='text']").on("keypress", function(e){
 		// getting text from input
 		var todoText = $(this).val();
 		// create new li and add to ul
-		$("ul").append("<li><span>X </span>" + todoText + "</li>");
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i> </span>" + todoText + "</li>");
 		// clear input field
 		$(this).val("");
 	};
+});
+
+// toggle form to hide/show when plus icon is clicked
+$("#plus-icon").on("click", function(){
+	$("input[type='text']").slideToggle(100)
 });
