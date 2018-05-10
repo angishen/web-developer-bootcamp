0446432c9a9505211f10f3ada510cbd0790ea496
Angela Shen <angela.shen@AWARDPINEBORDER.workdayinternal.com>
2018-05-09 17:05:05 -0700

    write jQuery event logic

---
+++ todo_list/assets/js/todo.js
@@ -0,0 +1,26 @@
+// check off todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("done")
+});
+
+// delete todos by clicking trashcan
+$("ul").on("click", "span", function(e){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove()
+	});
+	e.stopPropagation();
+});
+
+// add new to do
+$("input[type='text']").on("keypress", function(e){
+	
+	var liElement = document.createElement("li")
+	if (e.which === 13) {
+		// getting text from input
+		var todoText = $(this).val();
+		// create new li and add to ul
+		$("ul").append("<li><span>X </span>" + todoText + "</li>");
+		// clear input field
+		$(this).val("");
+	};
+});
